d96129987d5b10953582628ebdc2112ffcd04746
tkanta <tarininegi@gmail.com>
2012-07-03 17:18:27 +0530

    Invoicing path change

--- samples/updateInvoice.php
+++ samples/updateInvoice.php
@@ -1,7 +1,7 @@
 <?php
 $path = '../lib';
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-require_once('services/Invoice/InvoiceService.php');
+require_once('services/Invoicing/InvoiceService.php');
 require_once('PPLoggingManager.php');
 session_start();
 ?>
