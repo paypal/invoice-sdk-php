fbbef6990a94215c9f513749d4410293fbbbbb10
japatel <japatel@paypal.com>
2014-10-16 14:35:09 -0500

    Updated SDK Core to v2.5.*

--- lib/PayPal/Service/InvoiceService.php
+++ lib/PayPal/Service/InvoiceService.php
@@ -1,8 +1,10 @@
 <?php 
 namespace PayPal\Service;
+use PayPal\Common\PPApiContext;
 use PayPal\Core\PPMessage;
 use PayPal\Core\PPBaseService;
 use PayPal\Core\PPUtils;
+use PayPal\Handler\PPPlatformServiceHandler;
 use PayPal\Types\PT\CreateInvoiceResponse;
 use PayPal\Types\PT\SendInvoiceResponse;
 use PayPal\Types\PT\RemindInvoiceResponse;
@@ -38,9 +40,7 @@ class InvoiceService extends PPBaseService {
     *
     */
 	public function __construct($config = null) {
-		parent::__construct(self::$SERVICE_NAME, 'NV', array('PayPal\Handler\PPPlatformServiceHandler'), $config);
-        parent::$SDK_NAME    = self::$SDK_NAME ;
-        parent::$SDK_VERSION = self::$SDK_VERSION;
+		parent::__construct(self::$SERVICE_NAME, 'NV', $config);
 	}
 
 
@@ -55,194 +55,242 @@ class InvoiceService extends PPBaseService {
 	 */
 	public function CreateInvoice($createInvoiceRequest, $apiCredential = NULL) {
 		$ret = new CreateInvoiceResponse();
-		$resp = $this->call('Invoice', 'CreateInvoice', $createInvoiceRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'CreateInvoice', $createInvoiceRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: SendInvoice
 	 * @param SendInvoiceRequest $sendInvoiceRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\SendInvoiceResponse
 	 * @throws APIException
 	 */
 	public function SendInvoice($sendInvoiceRequest, $apiCredential = NULL) {
 		$ret = new SendInvoiceResponse();
-		$resp = $this->call('Invoice', 'SendInvoice', $sendInvoiceRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'SendInvoice', $sendInvoiceRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: RemindInvoice
 	 * @param RemindInvoiceRequest $remindInvoiceRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\RemindInvoiceResponse
 	 * @throws APIException
 	 */
 	public function RemindInvoice($remindInvoiceRequest, $apiCredential = NULL) {
 		$ret = new RemindInvoiceResponse();
-		$resp = $this->call('Invoice', 'RemindInvoice', $remindInvoiceRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'RemindInvoice', $remindInvoiceRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: CreateAndSendInvoice
 	 * @param CreateAndSendInvoiceRequest $createAndSendInvoiceRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\CreateAndSendInvoiceResponse
 	 * @throws APIException
 	 */
 	public function CreateAndSendInvoice($createAndSendInvoiceRequest, $apiCredential = NULL) {
 		$ret = new CreateAndSendInvoiceResponse();
-		$resp = $this->call('Invoice', 'CreateAndSendInvoice', $createAndSendInvoiceRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'CreateAndSendInvoice', $createAndSendInvoiceRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: UpdateInvoice
 	 * @param UpdateInvoiceRequest $updateInvoiceRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\UpdateInvoiceResponse
 	 * @throws APIException
 	 */
 	public function UpdateInvoice($updateInvoiceRequest, $apiCredential = NULL) {
 		$ret = new UpdateInvoiceResponse();
-		$resp = $this->call('Invoice', 'UpdateInvoice', $updateInvoiceRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'UpdateInvoice', $updateInvoiceRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: GetInvoiceDetails
 	 * @param GetInvoiceDetailsRequest $getInvoiceDetailsRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\GetInvoiceDetailsResponse
 	 * @throws APIException
 	 */
 	public function GetInvoiceDetails($getInvoiceDetailsRequest, $apiCredential = NULL) {
 		$ret = new GetInvoiceDetailsResponse();
-		$resp = $this->call('Invoice', 'GetInvoiceDetails', $getInvoiceDetailsRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'GetInvoiceDetails', $getInvoiceDetailsRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: CancelInvoice
 	 * @param CancelInvoiceRequest $cancelInvoiceRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\CancelInvoiceResponse
 	 * @throws APIException
 	 */
 	public function CancelInvoice($cancelInvoiceRequest, $apiCredential = NULL) {
 		$ret = new CancelInvoiceResponse();
-		$resp = $this->call('Invoice', 'CancelInvoice', $cancelInvoiceRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'CancelInvoice', $cancelInvoiceRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: SearchInvoices
 	 * @param SearchInvoicesRequest $searchInvoicesRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\SearchInvoicesResponse
 	 * @throws APIException
 	 */
 	public function SearchInvoices($searchInvoicesRequest, $apiCredential = NULL) {
 		$ret = new SearchInvoicesResponse();
-		$resp = $this->call('Invoice', 'SearchInvoices', $searchInvoicesRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'SearchInvoices', $searchInvoicesRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: MarkInvoiceAsPaid
 	 * @param MarkInvoiceAsPaidRequest $markInvoiceAsPaidRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\MarkInvoiceAsPaidResponse
 	 * @throws APIException
 	 */
 	public function MarkInvoiceAsPaid($markInvoiceAsPaidRequest, $apiCredential = NULL) {
 		$ret = new MarkInvoiceAsPaidResponse();
-		$resp = $this->call('Invoice', 'MarkInvoiceAsPaid', $markInvoiceAsPaidRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'MarkInvoiceAsPaid', $markInvoiceAsPaidRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: MarkInvoiceAsUnpaid
 	 * @param MarkInvoiceAsUnpaidRequest $markInvoiceAsUnpaidRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\MarkInvoiceAsUnpaidResponse
 	 * @throws APIException
 	 */
 	public function MarkInvoiceAsUnpaid($markInvoiceAsUnpaidRequest, $apiCredential = NULL) {
 		$ret = new MarkInvoiceAsUnpaidResponse();
-		$resp = $this->call('Invoice', 'MarkInvoiceAsUnpaid', $markInvoiceAsUnpaidRequest, $apiCredential);
+        $apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+        $resp = $this->call('Invoice', 'MarkInvoiceAsUnpaid', $markInvoiceAsUnpaidRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: MarkInvoiceAsRefunded
 	 * @param MarkInvoiceAsRefundedRequest $markInvoiceAsRefundedRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\MarkInvoiceAsRefundedResponse
 	 * @throws APIException
 	 */
 	public function MarkInvoiceAsRefunded($markInvoiceAsRefundedRequest, $apiCredential = NULL) {
 		$ret = new MarkInvoiceAsRefundedResponse();
-		$resp = $this->call('Invoice', 'MarkInvoiceAsRefunded', $markInvoiceAsRefundedRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'MarkInvoiceAsRefunded', $markInvoiceAsRefundedRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
-	 
+
 
 	/**
 	 * Service Call: DeleteInvoice
 	 * @param DeleteInvoiceRequest $deleteInvoiceRequest
 	 * @param mixed $apiCredential - Optional API credential - can either be
 	 * 		a username configured in sdk_config.ini or a ICredential object
-	 *      created dynamically 		
+	 *      created dynamically
 	 * @return Types\PT\DeleteInvoiceResponse
 	 * @throws APIException
 	 */
 	public function DeleteInvoice($deleteInvoiceRequest, $apiCredential = NULL) {
 		$ret = new DeleteInvoiceResponse();
-		$resp = $this->call('Invoice', 'DeleteInvoice', $deleteInvoiceRequest, $apiCredential);
+		$apiContext = new PPApiContext($this->config);
+        $handlers = array(
+            new PPPlatformServiceHandler($apiCredential, self::$SDK_NAME, self::$SDK_VERSION),
+        );
+		$resp = $this->call('Invoice', 'DeleteInvoice', $deleteInvoiceRequest, $apiContext, $handlers);
 		$ret->init(PPUtils::nvpToMap($resp));
 		return $ret;
 	}
