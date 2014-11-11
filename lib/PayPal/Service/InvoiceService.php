5033f044efaa04003d46c3efee3c4f62581aea74
japatel <japatel@paypal.com>
2014-11-11 14:25:11 -0600

    Updated Composer to use relaxed versions
    - Updated README with poodle update
    - Updated version number in User Agent

--- lib/PayPal/Service/InvoiceService.php
+++ lib/PayPal/Service/InvoiceService.php
@@ -33,7 +33,7 @@ class InvoiceService extends PPBaseService {
 	protected static $SDK_NAME = "invoice-php-sdk";
 	
 	// SDK Version
-	protected static $SDK_VERSION = "3.6.106";
+	protected static $SDK_VERSION = "3.6.107";
 
     /**
     * @param $config - Dynamic config map. This takes the higher precedence if config file is also present.
